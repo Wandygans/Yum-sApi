73702674490636c1942f6ebae23e6010fa01f11c
J <88314302+carlxjoe@users.noreply.github.com>
2022-09-03 14:52:32 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const secure = require('ssl-express-www')
+const PORT = process.env.PORT || 8080;
+const morgan = require('morgan')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+var app = express();
+//app.use(secure)
+app.use(morgan('dev'));
+app.use(express.static('client'));
+app.set("json spaces",2)
+__path = process.cwd()
+
+var downloader = require('./server/downloader.js');
+
+app.use('/downloader', downloader)
+
+app.listen(PORT, () => {
+    console.log(`Server Run on port ${PORT}`)
+});

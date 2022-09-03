19f256dd4458c815b3eda048449b233887296748
J <88314302+carlxjoe@users.noreply.github.com>
2022-09-03 15:15:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async(req, res) => {
-	res.sendFile(__path + '/views/index.html')
+	res.sendFile(__path + '/public/index.html')
 })
 /**router.get('/css/style.css', async(req, res) => {
 	res.sendFile(__path + '/views/css/style.css')
 })**/
 
-//Kalo page yang di cari engga ada, nanti muncul ini:v
+
 router.use(function (req, res) {
 res.status(404)
-.sendFile(__path + '/views/404.html')
+.sendFile(__path + '/public/404.html')
 });
 
 
